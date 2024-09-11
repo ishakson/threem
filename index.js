e8ac1cde7563808c7a22d8b1da11ae47e9362736
ishak <ishaksylmz7@gmail.com>
2024-09-11 16:47:33 +0300

    main

--- index.js
+++ index.js
@@ -4,58 +4,37 @@ import { OrbitControls } from "jsm/controls/OrbitControls.js";
 const w = window.innerWidth;
 const h = window.innerHeight;
 
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(75, w / h, 0.1, 1000);
+camera.position.z = 5;
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(w, h);
 document.body.appendChild(renderer.domElement);
 
-const fov = 75;
-const aspect = w / h;
-const near = 0.1;
-const far = 10;
-const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-camera.position.z = 2;
-
-const scene = new THREE.Scene();
-
 const controls = new OrbitControls(camera, renderer.domElement);
+
 controls.enableDamping = true;
-controls.dampingFactor = 0.05;
+controls.dampingFactor = 0.25;
 
-const geometry = new THREE.IcosahedronGeometry(1.0, 2);
+const loader = new THREE.TextureLoader();
+
+const geometry = new THREE.IcosahedronGeometry(1, 12);
 const material = new THREE.MeshStandardMaterial({
-  color: 0xffffff,
-  flatShading: true,
-});
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
-
-const wireMat = new THREE.MeshBasicMaterial({
-  color: 0xffffff,
-  wireframe: true,
-});
-const wireMesh = new THREE.Mesh(geometry, wireMat);
-wireMesh.scale.setScalar(1.001);
-mesh.add(wireMesh);
-
-const hemlight = new THREE.HemisphereLight(0x0099ff, 0xaa5500, 1);
-scene.add(hemlight);
-
-window.addEventListener("resize", () => {
-  const w = window.innerWidth;
-  const h = window.innerHeight;
-  renderer.setSize(w, h);
-  camera.aspect = w / h;
-  camera.updateProjectionMatrix();
+  color: 0xffff00,
+  map: loader.load("./earthmap1k.jpg"),
 });
 
+const earthMesh = new THREE.Mesh(geometry, material);
+scene.add(earthMesh);
+
+const hemLight = new THREE.HemisphereLight(0xffffff, 0x444444);
+scene.add(hemLight);
 function animate() {
   requestAnimationFrame(animate);
-  mesh.rotation.x += 0.001;
-  mesh.rotation.y += 0.001;
-
-  controls.update();
-
+  earthMesh.rotation.x += 0.001;
+  earthMesh.rotation.y += 0.001;
   renderer.render(scene, camera);
+  controls.update();
 }
-
 animate();
