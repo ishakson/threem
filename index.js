9c09a9c3f9a7ea2a7f15d802659d43844878e18a
ishak <ishaksylmz7@gmail.com>
2024-09-13 11:39:00 +0300

    main

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import * as Three from "three";
 import { OrbitControls } from "jsm/controls/OrbitControls.js";
 
+import spline from "./spline.js";
+
 const w = window.innerWidth;
 const h = window.innerHeight;
 const scene = new Three.Scene();
+scene.fog = new Three.FogExp2(0x000000, 0.3);
 const camera = new Three.PerspectiveCamera(75, w / h, 0.1, 1000);
 camera.position.z = 5;
 const renderer = new Three.WebGLRenderer();
@@ -14,25 +17,38 @@ const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
 controls.dampingFactor = 0.03;
 
-const geometry = new Three.BoxGeometry();
-const material = new Three.MeshStandardMaterial({
-  color: 0x00ff00,
+const points = spline.getPoints(100);
+const geometry = new Three.BufferGeometry().setFromPoints(points);
+const material = new Three.LineBasicMaterial({ color: 0xff0000 });
+const line = new Three.Line(geometry, material);
+
+const tubeGeometry = new Three.TubeGeometry(spline, 222, 0.65, 16, true);
+const tubeMaterial = new Three.MeshBasicMaterial({
+  color: 0x0000ff,
+  // side: Three.DoubleSide,
+  wireframe: true,
 });
-const cube = new Three.Mesh(geometry, material);
-scene.add(cube);
+const tube = new Three.Mesh(tubeGeometry, tubeMaterial);
+scene.add(line, tube);
 
-const hemLight = new Three.HemisphereLight(0xffffff, 0xffffff, 0.6);
-scene.add(hemLight);
 
-function animate() {
+function updateCamera(t) {
+  const time = t * 0.1;
+  const looptime = 10 * 1000;
+  const p = (time % looptime) / looptime;
+  const pos = tubeGeometry.parameters.path.getPointAt(p);
+  const lookAt = tubeGeometry.parameters.path.getPointAt((p + 0.03) % 1);
+  camera.position.copy(pos);
+  camera.lookAt(lookAt);
+}
+
+function animate(t = 0) {
   requestAnimationFrame(animate);
 
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-
+  updateCamera(t);
   renderer.render(scene, camera);
 
   controls.update();
 }
 
-animate();
+animate(0);
